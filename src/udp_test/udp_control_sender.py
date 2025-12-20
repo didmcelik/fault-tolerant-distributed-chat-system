ec507b618998e57b5b827e97905b39cfab102fa1
Didem Çelik <didem64600@gmail.com>
2025-12-20 12:12:47 +0200

    udp unicast test

---
+++ src/udp_test/udp_control_sender.py
@@ -0,0 +1,15 @@
+import socket
+import time
+
+TARGET_IP = "172.20.10.2"   # receiver IP
+CONTROL_PORT = 37021
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+i = 0
+while True:
+    msg = f"CONTROL_TEST {i}"
+    sock.sendto(msg.encode(), (TARGET_IP, CONTROL_PORT))
+    print(f"[SENDER] Sent: {msg}")
+    i += 1
+    time.sleep(1)
